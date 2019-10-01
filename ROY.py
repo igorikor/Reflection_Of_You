7c7c3b7dd9855a36991b803b2b99caef59808219
igorikor <i.shirokih@mail.ru>
2019-10-01 22:20:11 +0500

    Weather, icon, time, date is working

--- ROY.py
+++ ROY.py
@@ -3,28 +3,54 @@ from PIL import Image, ImageTk
 import datetime as dt
 import requests
 
+img = ""
+
+weather_icon_lib = {
+	'clear-day':'weather_icons\\sun.png',
+	'clear-night':'weather_icons\\sun.png',
+	'rain':'weather_icons\\rainy.png',
+	'snow':'weather_icons\\rainy.png',
+	'wind':'weather_icons\\cloudy.png',
+	'fog':'weather_icons\\cloudy.png',
+	'cloudy':'weather_icons\\cloudy.png',
+	'partly-cloudy-day':'weather_icons\\cloudy.png',
+	'partly-cloudy-night':'weather_icons\\cloudy.png'
+}
+
 #Функции
 
 #Получение данных
-def weather():
-	token = '5450a4138a3e282e322f96a369ce7c30'
+def weather(token):
 	data = requests.get("http://ipinfo.io/json").json()
 	location = data['loc'].split(',')
 	location = list(map(float, location))
 	weather = requests.get("https://api.darksky.net/forecast/%s/%f,%f" % (token, location[0], location[1])).json()
+
 	temp = weather['currently']['temperature']
+	weather_type = weather['currently']['icon']
+	
 	temp = (temp-32) * 5/9
-	return str(int(temp))
+	return str(int(temp)) +' '+ weather_type
+
+def img_parse(icon,w_lib=weather_icon_lib):
+	global img
+	stim = (w_lib[icon])
+	PIL_Image = Image.open(stim)
+	PIL_image_small = PIL_Image.resize((120,120), Image.ANTIALIAS)
+	img = ImageTk.PhotoImage(PIL_image_small)
+	return img
 
 #Апдейты
 def update():
 	time.config(text = dt.datetime.now().strftime("%H:%M:%S"))
-	main.after(1000, update)
+	day_mounth.config(text = dt.datetime.now().strftime("%d.%m.%Y"))
+	main.after(200, update)
 
 def update_weather():
-	weather_val.config(text = weather())
-	main.after(15000, update)
-
+	temp = weather(token).split()
+	weather_val.config(text=temp[0]+"°С")
+	f_weather_img.config(image=img_parse(temp[1]))
+	main.after(5*60*1000, update)
 
 #Основная часть
 main = Tk()
@@ -32,7 +58,7 @@ main = Tk()
 #Описание переменных
 screen_width = main.winfo_screenwidth()
 screen_height = main.winfo_screenheight()
-
+token = '5450a4138a3e282e322f96a369ce7c30'
 
 
 #Полноэкранный режим + выход по нажатию Esc
@@ -40,23 +66,24 @@ main.attributes('-fullscreen', True)
 main.bind('<Escape>', lambda e: main.destroy())
 
 
-#Описание фреймов
+#------------------------------------------------------------------------------------------Описание фреймов----------------------------------------------------------------------------------------------
 f_top = Frame(main)
 
 #Погода
 f_tw = Frame(f_top)
-f_weather = Frame(f_tw, bg = 'black', width=3*(screen_width/4))
+f_weather = Frame(f_tw, bg = 'black', width=3*(screen_width/3))
 f_weather_val = Frame(f_weather,  bg = f_weather['bg'])
 f_weather_freespace = Frame(f_weather,  bg = f_weather['bg'])
 f_val_1 = Frame(f_weather_val, bg=f_weather['bg'])
 f_val_2 = Frame(f_weather_val, bg=f_weather['bg'])
 
 #Время
-f_time = Frame(f_tw, bg = 'black',width=screen_width/4)
+f_time = Frame(f_tw, bg = 'black',width=screen_width/10)
 
 f_de = Frame(f_top)
-
-f_date = Frame(f_de,bg = 'blue')
+#Дата
+f_date = Frame(f_de, bg = 'black')
+day_mounth = Label(f_weather_freespace, text="01.01.2002", fg='white', bg='black', font='Arial 65')
 f_events = Frame(f_de,bg = 'pink')
 
 f_bot = Frame(main,bg = 'yellow')
@@ -65,18 +92,14 @@ f_news = Frame(f_bot,bg = 'orange')
 
 f_space = Frame(f_bot,bg = 'black')
 
-#text
+#Content
 time = Label(f_time,bg=f_weather['bg'],text=dt.datetime.now().strftime("%H:%M:%S"),fg='white',font='Arial 100')
-weather_val = Label(f_val_1,bg=f_weather['bg'],text=weather()+"°C",fg='white',font='Arial 75')
 
-stim = ("weather_icons\\sun.png")
-PIL_Image = Image.open(stim)
-PIL_image_small = PIL_Image.resize((120,120), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(PIL_image_small)
-f_weather_img = Label(f_val_2, bg='black',image = img)
+weather_val = Label(f_val_1,bg=f_weather['bg'], fg='white',font='Arial 65')
+f_weather_img = Label(f_val_2, image=img_parse('clear-day') ,bg='black')
 
-#Упаковка
 
+#-------------------------------------------------------------------------------------------------Упаковка-----------------------------------------------------------------------------------------------
 #top
 f_top.pack(expand=1,fill=BOTH)
 
@@ -97,7 +120,10 @@ weather_val.place(relx=.5,rely=.5,anchor=CENTER)
 f_weather_img.place(relx=.5,rely=.5,anchor=CENTER)
 
 f_de.pack(expand=1,fill=BOTH)
+#date
 f_date.pack(side=LEFT,expand=1,fill=BOTH)
+day_mounth.place(relx=.5,rely=.5,anchor=CENTER)
+
 f_events.pack(side=LEFT,expand=1,fill=BOTH)
 
 f_bot.pack(expand=1,fill=BOTH)
@@ -105,6 +131,6 @@ f_news.pack(side=LEFT,expand=1,fill=BOTH)
 f_space.pack(side=LEFT,expand=1,fill=BOTH)
 
 main.after(1000, update)
-main.after(15000, update_weather)
+main.after(5000, update_weather)
 
 main.mainloop()
